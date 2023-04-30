e096d19b2b7a4c8cc6032d43180cb5aa49ba636b
sortheme <sortheme@gmail.com>
2023-04-30 08:23:03 +0430

    implementing read and add new row

---
+++ sheety-api-class.php
@@ -0,0 +1,88 @@
+<?php 
+namespace SorthSheetyApi ;
+
+class api{
+
+    private $base_url;
+    private $project;
+    private $sheet;
+    private $authorization=false;
+
+    function __construct(){
+
+    }
+
+    function get_time(){
+        return date('c');
+    }
+
+    function set_base_url($url){
+        $this->base_url = $url;
+    }
+
+    function set_project($project){
+        $this->project = $project;
+    }
+
+    function set_sheet($sheet){
+        
+        $this->sheet = $sheet;
+    }
+    function set_authorization($authorization){
+        $this->authorization = $authorization;
+    }
+
+    private function get_full_url(){
+        return $this->base_url.'/'.$this->project.'/'.$this->sheet;
+    }
+
+    function get_sheet_data(){
+        /** for getting data we dont need content-type header */
+        $url = $this->get_full_url();
+        $headers = array();
+        $auth = !empty($this->authorization) ? $this->authorization : '';
+        if($auth){
+            $headers[] = $auth;
+        }
+        $data = $this->curl($url, 'GET', $headers, '');
+        return $data;
+    }
+
+    function add_new_row_to_sheet($fields){
+        $url = $this->get_full_url();
+        $headers = array(
+            'Content-Type: application/json'
+        );
+        $auth = !empty($this->authorization) ? $this->authorization : '';
+        if($auth){
+            $headers[] = $auth;
+        }
+        
+        $data = $this->curl($url, 'POST', $headers, $fields);
+        return $data;
+    }
+
+    private function curl($url, $type='POST', $headers=array(), $fields=''){
+        $curl = curl_init();
+        $args = array(
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        );
+        $args[CURLOPT_URL] = $url;
+        $args[CURLOPT_HTTPHEADER] = $headers;
+        $args[CURLOPT_CUSTOMREQUEST] = $type;
+        $args[CURLOPT_POSTFIELDS] = json_encode($fields);
+        curl_setopt_array($curl, $args);
+        $response = curl_exec($curl);
+        return $response;
+    }
+
+
+
+
+
+}
